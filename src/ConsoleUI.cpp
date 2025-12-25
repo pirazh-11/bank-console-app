59806e5a5580e61c813438d5405af16881603cce
pirazh-11 <rylkonikita48@gmail.com>
2025-12-25 23:05:36 +0300

    Update ConsoleUI.cpp

--- src/ConsoleUI.cpp
+++ src/ConsoleUI.cpp
@@ -1,2 +1,31 @@
 #include "ConsoleUI.h"
+#pragma once
+#include <string>
+#include "Bank.h"
+
+class ConsoleUI
+{
+private:
+    Bank& bank;
+
+    void clearInputBuffer();
+    int inputInt(const std::string& prompt);
+    double inputDouble(const std::string& prompt);
+    std::string inputString(const std::string& prompt);
+
+public:
+    ConsoleUI(Bank& b);
+
+    void showMainMenu();
+    void createAccount();
+    void showBalance();
+    void depositMoney();
+    void withdrawMoney();
+    void transferMoney();
+    void showAllAccounts();
+};
+
+
+
+
 
